f9521db2dbedd352d1d7e794f2c33526a83d302e
Thea Win <winwinkhaing.wk.14@gmail.com>
2024-08-19 19:17:23 +0400

    implementing chat user interface using Gifted Chat

--- components/Chat.js
+++ components/Chat.js
@@ -1,17 +1,68 @@
-import { useEffect } from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { useEffect, useState } from "react";
+import { GiftedChat, Bubble } from "react-native-gifted-chat";
+import { StyleSheet, View, KeyboardAvoidingView, Platform } from "react-native";
 
 export default function Chat({ route, navigation }) {
-
-const { name, color } = route.params;
+  const [messages,setMessages] = useState([]);
+  const { name, color } = route.params;
   
+  const onSend = (newMessages) => {
+    setMessages(previousMessages => GiftedChat.append(previousMessages, newMessages))
+  };
+
+  const renderBubble = (props) => {
+    return <Bubble
+    {...props}
+      wrapperStyle={{
+        right: {
+          backgroundColor: '#000'
+        },
+        left: {
+          backgroundColor: '#fff'
+        }
+      }}
+    />
+  }
+
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: 'Welcome to the Realm Chat',
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: 'React Native',
+          avatar: 'https://placeimg.com/140/140/any',
+        },
+      },
+      {
+        _id: 2,
+        text: 'You\'ve entered the chat',
+        createdAt: new Date(),
+        system: true,
+      },
+    ]);
+  }, []);
+
   useEffect(() => {
     navigation.setOptions({title: name});
   }, []);
 
   return (
     <View style={[styles.container, {backgroundColor: color}]}>
-      <Text style={styles.text}>Hello! Let's start chatting</Text>
+      <GiftedChat
+        messages={messages}
+        renderBubble={renderBubble}
+        onSend={(messages) => onSend(messages)}
+        user={{
+          _id: 1
+        }}
+      />
+      { Platform.OS === 'android' || Platform.OS === 'ios' 
+        ? <KeyboardAvoidingView behavior="height" />
+        : null 
+      }
     </View>
   );
 }
@@ -19,10 +70,5 @@ const { name, color } = route.params;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  text: {
-    color: '#fff'
   }
 })
