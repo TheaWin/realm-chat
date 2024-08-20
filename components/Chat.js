33e329e4cd158c655d3c1b8b91274ed7297c2d76
Thea Win <winwinkhaing.wk.14@gmail.com>
2024-08-20 10:04:00 +0400

    fetch and save message from database in real time

--- components/Chat.js
+++ components/Chat.js
@@ -1,14 +1,41 @@
 import { useEffect, useState } from "react";
 import { GiftedChat, Bubble } from "react-native-gifted-chat";
 import { StyleSheet, View, KeyboardAvoidingView, Platform } from "react-native";
+import { collection, addDoc, onSnapshot, query, orderBy } from "firebase/firestore";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-export default function Chat({ route, navigation }) {
+export default function Chat({ route, navigation, db }) {
   const [messages,setMessages] = useState([]);
-  const { name, color } = route.params;
+  const { userID, name, color } = route.params;
   
   const onSend = (newMessages) => {
-    setMessages(previousMessages => GiftedChat.append(previousMessages, newMessages))
+    // setMessages(previousMessages => GiftedChat.append(previousMessages, newMessages))
+    addDoc(collection(db, 'messages'), newMessages[0])
   };
+
+  useEffect(() => {
+    const q = query(
+      collection(db, 'messages'),
+      orderBy('createdAt', 'desc')
+    );
+    const unsubMessages = onSnapshot(
+      q,(documentsSnapshot) => {
+        let newMessages = [];
+        documentsSnapshot.forEach(doc => {
+          newMessages.push({
+            id: doc.id,
+            ...doc.data(),
+            createdAt: new Date(doc.data().createdAt.toMillis()),
+          })
+        });
+        setMessages(newMessages);
+      }
+    );
+    // Clean up code
+    return () => {
+      if (unsubMessages) unsubMessages();
+    }
+  }, []);
 
   const renderBubble = (props) => {
     return <Bubble
@@ -56,7 +83,8 @@ export default function Chat({ route, navigation }) {
         renderBubble={renderBubble}
         onSend={(messages) => onSend(messages)}
         user={{
-          _id: 1
+          _id: userID,
+          name: name,
         }}
       />
       { Platform.OS === 'android' || Platform.OS === 'ios' 
